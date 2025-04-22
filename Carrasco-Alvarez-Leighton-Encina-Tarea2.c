598ed7e9ff8f0b341759711d113cdbf7526741d8
OscarCA8 <oicarrasco@miuandes.cl>
2025-04-22 11:46:18 -0400

    Archivo con struct creada

---
+++ Carrasco-Alvarez-Leighton-Encina-Tarea2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct cancion {
+    char*song_id;
+    char*song_title;
+    char*artist;
+    char*album;
+    char*genre;
+    int*release_date;
+    int*duration;
+    int*popularity;
+    int*stream;
+    char*language;
+    char*explicit_content;
+    char*label;
+    char*composer;
+    char*producer;
+    char*collaboration;
+} cancion;
+
